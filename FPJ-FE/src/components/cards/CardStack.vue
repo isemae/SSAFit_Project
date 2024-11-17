35e50c9fc5a2915d2690c687bb66b4e9835a40a7
isemae <foxlis97@naver.com>
2024-11-17 11:37:28 +0900

    FPJ-42 Chore Change Card data source to exerciseStore

--- FPJ-FE/src/components/cards/CardStack.vue
+++ FPJ-FE/src/components/cards/CardStack.vue
@@ -1,19 +1,22 @@
 <template>
+  <RouterLink :to="{ name: 'profile' }">toProfile</RouterLink>
   <div class="card-container">
-    <Card v-for="item in store.userCollectedCardData" :key="item.id" :data="item" />
+    <Card v-for="item in exerciseStore.randomlySelectedExerciseData" :key="item.id" :data="item" />
   </div>
 </template>
 
 <script setup>
 import Card from './Card.vue'
 import { useCardStore } from '@/stores/cardStore'
+import { useExerciseStore } from '@/stores/exerciseStore'
 import { onBeforeMount } from 'vue'
-const store = useCardStore()
+const cardStore = useCardStore()
+const exerciseStore = useExerciseStore()
 
 onBeforeMount(async () => {
-  await store.getUserCollectedCardData(1)
-  await store.getUserRecentlyCollectedCardData(1)
-  await store.getRandomlySelectedExerciseData()
+  await cardStore.getUserCollectedCardData(1)
+  await cardStore.getUserRecentlyCollectedCardData(1)
+  await exerciseStore.getRandomlySelectedExerciseData()
 })
 </script>
 
