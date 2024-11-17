5e0468377eadd7a56f6f2756170e7155dcee6fda
NekoShoot <honggyunjo1109@gmail.com>
2024-11-17 22:20:09 +0900

    [FPJ-55] Feat get user total card count #56

---
+++ PJT-FINAL-J-JW-HG/src/main/java/com/ssafit/controller/UserController.java
@@ -0,0 +1,157 @@
+package com.ssafit.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafit.model.dto.User;
+import com.ssafit.model.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin(origins = {"http://localhost:5173/*", "http://localhost:5174/*"}) // TODO origin 허용할 uri 작성
+public class UserController {
+	private final UserService userService;
+	// 생성자로 의존성 주입
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+//////////////////////////////////////////////////////////////
+	// 1. 특정 유저 정보 전체 조회
+	// TODO 99. path variable -> localStorage에 저장돼있는 token을 활용해서 userId 보내기
+	@GetMapping("/{userId}")
+	public ResponseEntity<?> getUserInfo(@PathVariable int userId) {	
+		try {
+			// service 호출
+			User userInfo = userService.getUserInfo(userId);
+			
+			// 만약 service -> dao -> db 통신에서 실패한다면
+			if(userInfo == null) {
+				//TODO 99. 존재하지 않는 유저에 접근할 시 예외 처리 <- token의 userId로 접근할 경우엔 refresh 토큰과 비교하고 아닐 시로 변경 필요
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("유저 정보가 존재하지 않습니다.");			
+			}		
+				
+			// 정상 로직일 시
+			return new ResponseEntity<User>(userInfo, HttpStatus.OK);
+		} catch(Exception e) {
+			System.out.println("===userController===");
+			e.printStackTrace();
+			System.out.println("===userController===");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("비정상적인 접근입니다.");
+		}
+
+	}
+	
+	// 2. 유저의 건강력 조회
+	@GetMapping("/{userId}/score")
+	public ResponseEntity<?> getUserScore(@PathVariable int userId) {
+		// try catch로 비정상적인 접근 시 예외 처리
+		try {
+			// service 호출
+			int userScore = userService.getUserScore(userId);
+			
+			// 만약 service -> dao -> db 통신에서 실패한다면
+			if(userScore == -1) {
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("유저 정보가 존재하지 않습니다.");
+			}
+			
+			// 정상 로직일 시
+			return new ResponseEntity<Integer>(userScore, HttpStatus.OK);
+		}
+		catch(Exception e) {
+			System.out.println("===userController===");
+			e.printStackTrace();
+			System.out.println("===userController===");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("비정상적인 접근입니다.");
+		}		
+		
+	}
+	
+	// 3. 유저가 건강 관리한 연속 일수
+	@GetMapping("/{userId}/streak")
+	// TODO TypeMismatchException 처리 
+	public ResponseEntity<?> getUserStreak(@PathVariable int userId) {
+		try {
+			// service 호출
+			int userStreak = userService.getUserStreak(userId);
+			
+			// 만약 service -> dao -> db 통신에서 실패한다면
+			if(userStreak == -1) {
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("유저 정보가 존재하지 않습니다.");
+			}
+			
+			// 정상 로직일 시
+			return new ResponseEntity<Integer>(userStreak, HttpStatus.OK);
+		}
+		catch(Exception e) {
+			System.out.println("===userController===");
+			e.printStackTrace();
+			System.out.println("===userController===");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("비정상적인 접근입니다.");
+		}		
+	}
+	
+	
+	// 4. 유저의 등급 조회
+	@GetMapping("/{userId}/tier")
+	public ResponseEntity<?> getUserTier(@PathVariable int userId) {
+		try {
+			// service 호출
+			int userTier = userService.getUserTier(userId);			
+			
+			// 만약 service -> dao -> db 통신에서 실패한다면
+			if(userTier == -1) {
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("유저 정보가 존재하지 않습니다.");
+			}
+			
+			// 정상 로직일 시
+			return new ResponseEntity<Integer>(userTier, HttpStatus.OK);
+		}
+		catch(Exception e) {
+			System.out.println("===userController===");
+			e.printStackTrace();
+			System.out.println("===userController===");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("비정상적인 접근입니다.");
+		}		
+	}
+	
+	// 5. 유저 등급 업데이트
+	@PutMapping("/{userId}/tier")
+	public ResponseEntity<?> updateUserTier(@PathVariable int userId, @RequestBody int userTier) {
+		//TODO int userTier -> JSON Parse Exception 수정
+		System.out.println(userTier);
+		return null;
+	}
+	
+	// 6. 유저가 획득한 총 카드 수 조회
+	@GetMapping("/{userId}/totalCardCount")
+	public ResponseEntity<?> getUserTotalCardCount(@PathVariable int userId) {
+		try {
+			// service 호출
+			int userTotalCardCount = userService.getUserTotalCardCount(userId);
+			
+			// 만약 service -> dao -> db 통신에서 실패한다면
+			if(userTotalCardCount == -1) {
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("유저 정보가 존재하지 않습니다.");
+			}
+			
+			// 정상 로직일 시
+			return new ResponseEntity<Integer>(userTotalCardCount, HttpStatus.OK);					
+		}
+		catch(Exception e) {
+			System.out.println("===userController===");
+			e.printStackTrace();
+			System.out.println("===userController===");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("비정상적인 접근입니다.");
+		}				
+	}
+	
+	// 7. 유저가 획득한 총 카드 수 업데이트
+}
---
+++ PJT-FINAL-J-JW-HG/src/main/java/com/ssafit/model/service/UserServiceImpl.java
@@ -0,0 +1,156 @@
+package com.ssafit.model.service;
+
+import org.springframework.stereotype.Service;
+
+import com.ssafit.model.dao.UserDao;
+import com.ssafit.model.dto.User;
+
+@Service
+public class UserServiceImpl implements UserService {
+	private final UserDao userDao;
+	// 생성자로 의존성 주입
+	public UserServiceImpl(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	// 1. 특정 유저 정보 전체 조회
+	/* return:
+	{ 
+	 	id,
+	 	loginId,
+		userName,
+		score,
+		totalCardCount,
+		tier
+	}
+	 */
+	@Override
+	public User getUserInfo(int userId) {
+		try {
+			// 정상 로직
+			User userInfo = userDao.getUserInfo(userId);
+			
+			// loginId, password는 db에서 가져오지 않음
+			// password는 절대 안되고 loginId는 필요시 가져오기 가능
+	
+			// 조회에 실패하면 null이 반환됨
+			// mapper의 resultType이 Object 이기 때문에 존재하지 않는 유저라도 null 반환할수도 있음.
+			if(userInfo == null) {
+				System.out.println("Service에서 통신. " + userId + "번 유저정보를 찾을 수 없습니다.");
+				return null;
+			}
+			
+			// token에 있는 id로 조회할 경우에는 없어도 갠춘한 로직일지도? token 탈취를 고려한다면 refresh 토큰 검증 필요
+			// 본인이 아닌 다른 사람의 정보를 조회할 경우
+			if(userInfo.getId() != userId) {
+				System.out.println("다른 사람의 정보를 조회하려 합니다.");
+				return null;
+			}		
+			
+			return userInfo;
+		}
+		catch(Exception e) {
+			// 기타 예외에 대해서도 null 반환
+			System.out.println("===userServiceImpl===");
+			e.printStackTrace();
+			System.out.println("===userServiceImpl===");
+			
+			return null;
+		}
+	}
+
+	// 2. 유저의 건강력 조회
+	/* return:
+	 (int) score
+	 */
+	@Override
+	public int getUserScore(int userId) {
+		try {
+			// 정상 로직
+			int userScore = userDao.getUserScore(userId);
+			
+			return userScore; 
+		}
+		catch(Exception e) {
+			System.out.println("===userServiceImpl===");
+			e.printStackTrace();
+			System.out.println("===userServiceImpl===");
+			
+			// -1 반환
+			return -1;
+		}
+	}
+
+	
+	// 3. 유저가 건강 관리한 연속 일수
+	/* return:
+	 (int) streak
+	 */
+	@Override
+	public int getUserStreak(int userId) {
+		try {
+			int userStreak = userDao.getUserStreak(userId);	
+			
+			return userStreak;
+		}
+		catch(Exception e) {
+			System.out.println("===userServiceImpl===");
+			e.printStackTrace();
+			System.out.println("===userServiceImpl===");
+			
+			return -1;
+		}				
+	}
+
+	
+	// 4. 유저의 등급 조회
+	/* return:
+	 (int) tier
+	 */
+	@Override
+	public int getUserTier(int userId) {
+		try {
+			// 정상 로직
+			int userTier = userDao.getUserTier(userId);
+			
+			return userTier;
+		}
+		catch(Exception e) {
+			System.out.println("===userServiceImpl===");
+			e.printStackTrace();
+			System.out.println("===userServiceImpl===");
+			
+			return -1;
+		}		
+	}
+	
+	// 5. 유저 등급 업데이트
+	@Override
+	public int updateUserTier(int userId, int userTier) {
+		return 0;
+	}
+	
+	
+	// 6. 유저가 획득한 총 카드 수 조회
+	/* return:
+	 (int) totalCardCount
+	 */
+	@Override
+	public int getUserTotalCardCount(int userId) {
+		try {
+			// 정상 로직
+			int userTotalCardCount = userDao.getUserTotalCardCount(userId);
+			
+			return userTotalCardCount;					
+		}
+		catch(Exception e) {
+			System.out.println("===userServiceImpl===");
+			e.printStackTrace();
+			System.out.println("===userServiceImpl===");
+			
+			return -1;
+		}		
+	}
+
+
+}
